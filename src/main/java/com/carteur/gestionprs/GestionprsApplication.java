5e799efcd6145d326cbfcc40351969250ecd4d78
carteur007 <julienmarsellas@gmail.com>
2021-08-03 09:52:58 +0200

    Creation du projet de  gestion du personnel

---
+++ src/main/java/com/carteur/gestionprs/GestionprsApplication.java
@@ -0,0 +1,27 @@
+package com.carteur.gestionprs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionprsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionprsApplication.class, args);
+	}
+	@GetMapping("/hello")
+		public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}/*
+	@Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }*/
+
+}
---
+++ src/test/java/com/carteur/gestionprs/GestionprsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carteur.gestionprs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionprsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
